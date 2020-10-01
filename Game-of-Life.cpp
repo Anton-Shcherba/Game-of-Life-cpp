86c255b660e88ed02af165ae374e2244b8ec47cb
TONY <tony.shcherba@gmail.com>
2020-10-01 13:38:41 +0300

    Create Game-of-Life.cpp
    Чтение из файла

---
+++ Game-of-Life.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <windows.h>
+
+int main()
+{
+    //ПЕРЕМЕННЫЕ
+    struct cell {
+        bool live;
+        unsigned neighbors;
+    };
+
+    cell field[102][102];
+    for (int m = 0; m < 102; m++) for (int n = 0; n < 102; n++) {
+        field[m][n].live = false;
+        field[m][n].neighbors = 0;
+    }
+
+    int M, N;
+
+    //ЧТЕНИЕ
+    ifstream file("life.txt");
+    file >> M >> N;
+
+    file.get();
+    for (int m = 1; m <= M; m++) {
+        for (int n = 1; n <= N; n++) {
+            int t = file.get();
+            if (t != 10) field[m][n].live = t - 48;
+        }
+        file.get();
+    }
+    file.close();
+}
