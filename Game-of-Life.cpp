586db6cee5daa34cd9acd76e2fc94a65b63609ad
TONY <tony.shcherba@gmail.com>
2020-10-01 14:53:50 +0300

    Update Game-of-Life.cpp
    Учёт количества живых клеток на каждом поколении.

--- Game-of-Life.cpp
+++ Game-of-Life.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 int main()
 {
+    int moves; //количесто ходов
+    cout << "Enter the number of moves: " << endl;
+    cin >> moves;
+
     //СТРУКТУРА КЛЕТКИ
     struct cell {
         bool live; //состояние
@@ -12,20 +16,24 @@ int main()
     };
 
     cell field[102][102]; //макс.100х100 + рамка из нулей
-    for (int m = 0; m < 102; m++) for (int n = 0; n < 102; n++) {
+    for (int m = 0; m < 102; m++) for (int n = 0; n < 102; n++) { //заполнение матрицы нолями
         field[m][n].live = false;
         field[m][n].neighbors = 0;
     }
 
     //ЧТЕНИЕ
     int M, N;
+    int living_cells = 0;
     ifstream file("life.txt");
     file >> M >> N;
     file.get();
     for (int m = 1; m <= M; m++) {
         for (int n = 1; n <= N; n++) {
             int t = file.get();
-            if (t != 10) field[m][n].live = t - 48;
+            if (t != 10) { //исключая перенос строки
+                field[m][n].live = t - 48;
+                living_cells += t - 48;
+            }
         }
         file.get();
     }
@@ -45,18 +53,16 @@ int main()
         field[m][n].neighbors = tmp;
     }
 
-    //ПЕРЕСЧЕТ ЖИВЫХ КЛЕТОК
+    //ПЕРЕСЧЕТ(ПОДСЧЁТ) ЖИВЫХ КЛЕТОК 
     for (int m = 1; m <= M; m++) for (int n = 1; n <= N; n++) {
-        if (field[m][n].live && field[m][n].neighbors != 2 && field[m][n].neighbors != 3) field[m][n].live = false;
-        if (!field[m][n].live && field[m][n].neighbors == 3) field[m][n].live = true;
-    }
-
-    //ВЫВОД 
-    for (int m = 1; m <= M; m++) {
-        for (int n = 1; n <= N; n++) {
-            if (field[m][n].live) cout << (char)254 << " ";
-            else cout << "  ";
+        if (field[m][n].live && field[m][n].neighbors != 2 && field[m][n].neighbors != 3) {
+            living_cells--;
+            field[m][n].live = false;
         }
-        cout << endl;
+        if (!field[m][n].live && field[m][n].neighbors == 3) {
+            field[m][n].live = true;
+            living_cells++;
+        }
     }
+    return 0;
 }
